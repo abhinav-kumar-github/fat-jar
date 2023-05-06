69e3d5249fd11e09b6fd112239907df485c77b74
Abhinav Kumar <abhinav88733@gmail.com>
2023-05-07 00:12:57 +0530

    Argument from terminal

--- src/main/java/me/abhinav/Main.java
+++ src/main/java/me/abhinav/Main.java
@@ -1,19 +1,17 @@
 package me.abhinav;
 
+import okhttp3.OkHttpClient;
+
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // java -jar build/libs/task-management-1.0-SNAPSHOT.jar argument1 argument2 argument3
+        System.out.println("abhinav okHttpClient tar !!!");
+        System.out.println("arguments " + Arrays.toString(args));
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        OkHttpClient okHttpClient = new OkHttpClient();
     }
 }
--- src/main/java/me/abhinav/greeting/Greeting.java
+++ src/main/java/me/abhinav/greeting/Greeting.java
@@ -1,2 +1,12 @@
-package me.abhinav.greeting;public class Greeting {
+package me.abhinav.greeting;
+
+import okhttp3.OkHttpClient;
+
+public class Greeting {
+    public static void Greet(String value) {
+
+    }
+    public static void main(String[] args) {
+        System.out.printf("abhinav greeting tar !!!");
+    }
 }
